7f36965b0c871f55ae0ed60e44ea1c155c4bcbe6
JelenaZoric <zoric.jelena95@gmail.com>
2018-09-05 04:49:20 +0200

    pregled rezervacija

--- src/app/ireservation.ts
+++ src/app/ireservation.ts
@@ -1,10 +1,11 @@
 import {AccommodationComponent} from './accommodation/accommodation.component';
 import {UserComponent} from './user/user.component';
+import {IAccommodation} from './iaccommodation';
 
 export interface IReservation {
   dateFrom: string;
   dateTo: string;
   status: number;
-  accommodation: AccommodationComponent;
+  accommodation: IAccommodation;
   user: UserComponent;
 }
